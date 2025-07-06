17d7a87e63a601acacb58cbb30f0efd78faa65c6
Sina Ghaderi <82523695+sighaderi@users.noreply.github.com>
2025-07-06 21:43:40 +0200

    fix: comment out logging filename in MainController.py

--- MainController.py
+++ MainController.py
@@ -31,7 +31,8 @@ import datetime
 # Enable logging
 log.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                 level=log.INFO,
-                filename='../logs/logging.log')
+                # filename='../logs/logging.log'
+                )
 
 logger = log.getLogger(__name__)
 
